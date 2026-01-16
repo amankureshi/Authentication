9e387f34e7e4c64c7e7425468e3cdafb193db869
Aman kureshi <amankureshi78692@gmail.com>
2026-01-16 22:20:37 +0530

    Create register user API logic

---
+++ server/controllers/authController.js
@@ -0,0 +1,8 @@
+import userModels from "../models/userModels";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, passsword } = req.body;
+    }
+}
